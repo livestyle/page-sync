2873dfb6ffba07153303944a008eb6d0d95f786e
Sergey Chikuyonok <serge.che@gmail.com>
2016-01-05 01:16:27 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 'use strict';
 
+import EventEmitter from 'eventemitter3';
 import * as events from './lib/events';
 
-export {events};
+const eventList = Object.keys(events).map(key => events[key]);
+
+export function host(doc, options={}) {
+	let out = new EventEmitter();
+	let proxy = data => out.emit('data', data);
+	let onUnload = evt => {
+		if (evt.target === doc) {
+			out.dispose();
+		}
+	};
+	let bindings = eventList.map(e => e.host(doc, options).on('data', proxy));
+	out.dispose = () => {
+		bindings.forEach(binding => {
+			binding.off('data', proxy);
+			if (binding.dispose) {
+				binding.dispose();
+			}
+		});
+		bindings.length = 0;
+		doc.defaultView.removeEventListener('unload', onUnload);
+		out.emit('dispose');
+	};
+
+	doc.defaultView.addEventListener('unload', onUnload);
+	return out;
+}
+
+export function guest(doc, options={}) {
+	let bindings = eventList.map(e => e.guest(doc, options));
+	let out = data => bindings.forEach(b => b(data));
+	out.dispose = () => {
+		bindings.forEach(b => b.dispose && b.dispose());
+		bindings.length = 0;
+	};
+	return out;
+}
